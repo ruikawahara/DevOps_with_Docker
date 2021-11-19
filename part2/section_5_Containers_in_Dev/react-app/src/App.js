aa4a8f2fa9b7212d1a1e690d45efce4fc9a40247
Rui Kawahara <rkawahara@alaska.edu>
2021-11-19 02:52:43 -0900

    finished 2.11. need to update README

--- part2/section_5_Containers_in_Dev/react-app/src/App.js
+++ part2/section_5_Containers_in_Dev/react-app/src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  // for input fields
+  const [num1, setNum1] = useState(0)
+  const [num2, setNum2] = useState(0)
+
+  // for solution
+  const [ans, setAns] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Not efficient, but works
-          {/* Edit <code>src/App.js</code> and save to reload. */}
+          <u><b>Simple Calculator App</b></u>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <body>
+        <div>
+          <p>First...</p>
+          <input 
+            name="num1" 
+            type="number"
+            pattern="[0-9]+"
+            onChange={e => setNum1(e.target.value)}
+            placeholder="Enter first number"
+          />
+          <p> then, ...</p>
+          <input 
+            name="num2" 
+            type="number"
+            pattern="[0-9]+"
+            onChange={e => setNum2(e.target.value)}
+            placeholder="Enter second number"
+          />
+
+          <div>
+            <button onClick={() => setAns(parseInt(num1) + parseInt(num2))}>
+              +
+            </button>
+          </div>
+
+          <div>
+            <button onClick={() => setAns(parseInt(num1) - parseInt(num2))}>
+              -
+            </button>
+          </div>
+
+          <div>
+            <button onClick={() => setAns(parseInt(num1) * parseInt(num2))}>
+              *
+            </button>
+          </div>
+
+          <div>
+            <button onClick={() => setAns(parseInt(num1) / parseInt(num2))}>
+              /
+            </button>
+          </div>
+
+          <p>Answer is: {ans}.</p>
+
+        </div>
+      </body>
     </div>
   );
 }
